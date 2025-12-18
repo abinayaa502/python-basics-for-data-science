ef784833552a591754b166ceca992fd85bd7ea7e
ABINAYA R <abeee502@gmail.com>
2025-12-18 19:40:44 +0530

    Add variables example for python basics

---
+++ basics/variables.py
@@ -0,0 +1,8 @@
+name = "Abinaya"
+age = 20
+course = "B.Tech AI & Data Science"
+college = "Kumaraguru College of Technology"
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("College:", college)
